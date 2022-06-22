ba46986f509fa5178450ec538e8d6c9b084d3020
KCH6937 <adasddasd12@naver.com>
2022-06-22 22:29:44 +0900

    타입스크립트에서 선언하는 함수의 형식

---
+++ class-note/2_functions.ts
@@ -0,0 +1,14 @@
+// 함수에 타입에 타입을 정의하는 방식
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+sum(10, 20);
+
+// 함수의 옵셔널 파라미터(?)
+function log(a: string, b?: string, c?: string) {
+
+}
+
+log('hello world');
+log('hello ts', 'abc');
