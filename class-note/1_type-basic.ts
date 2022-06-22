03f5e031835fb7df8806fc3fb1885fe0bc9e050b
KCH6937 <adasddasd12@naver.com>
2022-06-22 22:29:30 +0900

    타입스크립트에서 지원하는 타입들

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,33 @@
+// TS 문자열 선언
+let str: string = 'hello';
+
+
+// TS 숫자 선언
+let num: number = 10;
+
+
+// TS 배열 선언
+let arr: Array<number> = [1, 2, 3];
+// 또는
+let arr2: number[] = [1, 2, 3];
+
+
+// TS 튜플
+let address: [string, number] = ['stringType', 100];
+
+
+// TS 객체
+let obj: Object = {};
+
+let person: object = {
+    name: 'capt',
+    age: 100
+};
+// 또는
+let person2: { name: string, age: number } = {
+    name: 'capt',
+    age: 100
+};
+
+// TS 진위값
+let show: boolean = true;
