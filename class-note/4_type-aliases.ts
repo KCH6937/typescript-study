7d14cd64c38650ce250242d216670ca0dd8a0494
KCH6937 <adasddasd12@naver.com>
2022-06-27 18:35:33 +0900

    인터페이스와 타입앨리어스(type)의 차이점

--- class-note/4_type-aliases.ts
+++ class-note/4_type-aliases.ts
@@ -1,3 +1,22 @@
-type Person = {
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+
+type Person2 = {
+    name: string;
+    age: number;
+}
+
+var seho: Person2 = {
+    name: '세호',
+    age: 30
+};
+
+type MyString = string;
+var mystr: MyString = 'hello';
+
+type Todo = { id: string; title: string, done: boolean };
+function getTodo(todo: Todo) {
     
 }
